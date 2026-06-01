9dba75f994feeedb8d89fa0c86a255057934e1a5
qxnt41oz8 <chino.10.28@icloud.com>
2026-06-01 05:16:41 +0000

    interface:pyロボットをサイボーグに変更

--- interface.py
+++ interface.py
@@ -6,7 +6,7 @@ client = OpenAI(
 )
 
 messages = [
-    {"role": "system", "content": "あなたは詳細に答えるアシスタントですにゃ。"},
+    {"role": "system", "content": "あなたはロボットです。"},
 ]
 
 print("bot: 起動しました。exit で終了します。")
